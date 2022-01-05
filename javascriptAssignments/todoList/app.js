35390e78fa87b497d72c05e3489a375e42293cfb
Toby <tobywas@gmail.com>
2022-01-04 22:25:33 -0500

    cant get drag and drop to work HELP!!

--- javascriptAssignments/todoList/app.js
+++ javascriptAssignments/todoList/app.js
@@ -2,16 +2,53 @@ let taskTextBox = document.getElementById("taskTextBox");
 let btnAdd = document.getElementById("btnAdd");
 let pendingTasks = document.getElementById("pendingTasks");
 let completedTasks = document.getElementById("completedTasks");
-const draggables = document.querySelectorAll('.draggable')
-const container = document.querySelectorAll('.container')
+const draggables = document.querySelectorAll(".draggable");
+const container = document.querySelectorAll(".container");
 
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
+  });
 
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.remove("dragging");
+  });
+});
 
+container.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector(".dragging");
+    console.log(draggable);
+    if (afterElement == null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  });
+});
 
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll(".draggable:not(.dragging)"),
+  ];
+
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
+}
 
 btnAdd.addEventListener("click", function () {
-
-    
   let task = taskTextBox.value;
 
   let checkBox = document.createElement("input");
@@ -38,43 +75,14 @@ btnAdd.addEventListener("click", function () {
     }
   });
 
-  let labelItem = document.createElement("labelItem");
+  let labelItem = document.createElement("p");
   labelItem.innerHTML = task;
 
-  
+  taskLi.setAttribute("class", "draggable");
+  taskLi.setAttribute("draggable", "true");
 
   pendingTasks.appendChild(taskLi);
   taskLi.appendChild(checkBox);
   taskLi.appendChild(labelItem);
   taskLi.appendChild(removeBtn);
-  labelItem.appendChild(task);
 });
-
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging')
-    })
-
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.remove('dragging')
-    })
-
-})
-
-
-// container.forEach(container => {
-//     container.addEventListener('dragover', e => {
-//         e.preventDefault()
-//         const afterElement = getDragAfterElement(container, e.clientY)
-//         const draggable = document.querySelector('.dragging')
-//         container.appendChild(draggable)
-//     })
-// })
-
-// function getDragAfterElement(container, y) {
-//     const draggableElements = [...container.querySelectorAll('draggable:not(.dragging)')]
-
-//     draggableElements.reduce(closest, child) => {
-//         const box = child.getBoundingClientRect()
-//     }, {offset: Number.POSITIVE_INFINITY})
-// }
